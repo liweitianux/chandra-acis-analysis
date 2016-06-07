8a7654b008891f5c741822c56c8eeda52b1c0fbb
Aaron LI <aaronly.me@gmail.com>
2016-06-08 01:14:16 +0800

    Rewrite Makefile; also remove obsolete files

--- mass_profile/init.sh
+++
@@ -1,6 +0,0 @@
-ln -sf $XANBIN/lib/libcpgplot*.a ./libcpgplot.a
-ln -sf $XANBIN/lib/libpgplot*.a ./libpgplot.a
-echo LIB ./ >pgplot_path.txt
-echo INC $XANBIN/include >>pgplot_path.txt
-
-chmod a+x *.py
