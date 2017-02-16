1b839fd299c77624bc9522995bc0241de63eee8f
Aaron LI <aaronly.me@outlook.com>
2017-02-16 23:41:11 +0800

    Remove calc_distance (last used by the deleted 'calc_lx')

--- mass_profile/calc_distance.h
+++
@@ -1,8 +0,0 @@
-#ifndef CALC_DISTANCE_H
-#define CALC_DISTANCE_H
-extern double calc_angular_distance(double z);
-extern double calc_luminosity_distance(double z);
-extern double E(double z);
-#endif
-
-//EOF
