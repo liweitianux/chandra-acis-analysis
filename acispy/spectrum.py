4fecedac4f65e453df3d7acc93d940c192b2421b
Aaron LI <aaronly.me@outlook.com>
2017-03-03 20:33:00 +0800

    acispy/spectrum.py: Add method "calc_flux()"

--- acispy/spectrum.py
+++ acispy/spectrum.py
@@ -27,18 +27,24 @@ class Spectrum:
         self.EXPOSURE = self.header.get("EXPOSURE")
         self.BACKSCAL = self.header.get("BACKSCAL")
 
-    def calc_pb_flux(self, elow=9500, ehigh=12000):
+    def calc_flux(self, elow, ehigh):
         """
-        Calculate the particle background flux:
+        Calculate the flux:
             flux = counts / exposure / area
 
         Parameters
         ----------
         elow, ehigh : float, optional
-            Lower and upper energy limit for the particle background.
+            Lower and upper energy limit to calculate the flux.
         """
         chlow = ACIS.energy2channel(elow)
         chhigh = ACIS.energy2channel(ehigh)
         counts = self.counts[(chlow-1):chhigh].sum()
         flux = counts / self.EXPOSURE / self.BACKSCAL
         return flux
+
+    def calc_pb_flux(self, elow=9500, ehigh=12000):
+        """
+        Calculate the particle background (default: 9.5-12 keV) flux.
+        """
+        return self.calc_flux(elow=elow, ehigh=ehigh)
